b9724721e724a47feaef80890afd921a4db93310
‘cc’ <2798750836@qq.com>
2022-12-19 15:10:48 +0800

    左侧导航条去除下划线,并引入顶部

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,7 @@ export default {
       margin:0;
       padding:0;
     }
+    a{
+      text-decoration-line: none;
+    }
 </style>
--- src/pages/Main/index.vue
+++ src/pages/Main/index.vue
@@ -3,9 +3,9 @@
     <el-container>
       <el-aside width="200px">
         <Left></Left>
-        </el-aside>
+      </el-aside>
       <el-container>
-        <el-header>Header</el-header>
+        <el-header><Header></Header></el-header>
         <el-main>Main</el-main>
       </el-container>
     </el-container>
@@ -15,9 +15,11 @@
 
 <script>
 import Left from "@/components/Left";
+import Header from "@/components/Header";
 export default {
   components: {
     Left,
+    Header,
   },
 };
 </script>
