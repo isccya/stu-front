a26c672d8e8f3ac9354fa7669b71d8a6abf178cd
‘cc’ <2798750836@qq.com>
2022-12-22 13:15:12 +0800

    配置部分vuex,和api请求

--- src/pages/showPerson/index.vue
+++ src/pages/showPerson/index.vue
@@ -7,6 +7,7 @@
       width="50%"
       :before-close="handleClose"
     >
+      <!-- before-close即为弹窗关闭时候 -->
       <el-form
         ref="form"
         :model="form"
@@ -34,48 +35,47 @@
           >
           </el-date-picker>
         </el-form-item>
-        <el-form-item label="籍贯" prop="addr">
-          <el-input
-            v-model="form.addr"
-            placeholder="请输入籍贯"
-          ></el-input>
+        <el-form-item label="籍贯" prop="nativePlace">
+          <el-input v-model="form.nativePlace" placeholder="请输入籍贯"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false"
-          >确 定</el-button
-        >
+        <el-button @click="cancel">取 消</el-button>
+        <el-button type="primary" @click="submit()">确 定</el-button>
       </span>
     </el-dialog>
+    <!-- <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="学号" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="name" label="性别" width="180"> </el-table-column>
+
+      <el-table-column prop="address" label="班级" width="180"> </el-table-column>
+      <el-table-column prop="name" label="生日" width="180"> </el-table-column>
+
+    </el-table> -->
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   data() {
     return {
-      dialogVisible: false, //显示新增内容的组件
+      //是否显示新增内容的组件
+      dialogVisible: false,
       //新增的表单里面数据
       form: {
         id: "",
         name: "",
         sex: 0,
         birthday: "",
-        addr: "",
+        nativePlace: "",
       },
       //rules限制添加信息
       rules: {
-        id: [
-          { required: true, message: "请输入学号" },
-          { type:'integer' ,message: '请输入整数', trigger: 'blur' }
-        ],
-        name: [
-          { required: true, message: "请输入姓名" },
-        ],
-        sex: [
-          { required: true, message: "请选择性别" },
-        ],
+        id: [{ required: true, message: "请输入学号" }],
+        name: [{ required: true, message: "请输入姓名" }],
+        sex: [{ required: true, message: "请选择性别" }],
         birthday: [
           {
             type: "date",
@@ -83,22 +83,43 @@ export default {
             message: "请选择日期",
           },
         ],
-        addr: [
-          { required: true, message: "请输入籍贯" },
-        ],
+        nativePlace: [{ required: true, message: "请输入籍贯" }],
       },
+      tableData:{
+
+      }
     };
   },
   methods: {
+    ...mapActions('showPerson',["addStudentList",'StudentList']),
+   
+    //单击弹窗对话(dialog)右上角关闭时候触发
     handleClose(done) {
-      this.$confirm("确认关闭？")
-        .then((_) => {
-          done();
-        })
-        .catch((_) => {});
+      this.$refs.form.resetFields();
+      this.dialogVisible = false;
     },
-    
+    // 点击取消时候触发
+    cancel() {
+      this.handleClose();
+    },
+    // 点击提交时候触发
+    submit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          alert("提交成功");
+        this.addStudentList(this.form);
+          this.$refs.form.resetFields();
+          this.dialogVisible = false;
+        }
+        if (!valid) {
+          alert("提交失败!");
+        }
+      });
+    },
   },
+  mounted(){
+    this.StudentList({current:1,size:5});
+  }
 };
 </script>
 
--- src/store/showPerson/index.js
+++ src/store/showPerson/index.js
@@ -1,16 +1,33 @@
+import { reqaddStudentList,reqStudentList } from '@/api/index'
 const actions = {
+    async addStudentList(context,form) {
 
+        let result = await reqaddStudentList(form);//最终的最终是在vuex里面发送请求.acitons再给mutations,mutations再把数据放入仓库中
+        console.log(form);
+        // if (result.code == 200) {
+            context.commit('ADDSTUDENTLIST', form)
+        // }
+    },
+    async StudentList(context,currentSize) {
+        let result = await reqStudentList(currentSize.current,currentSize.size);//最终的最终是在vuex里面发送请求.acitons再给mutations,mutations再把数据放入仓库中
+        if (result.code == 20000) {
+            context.commit('STUDENTLIST', result.data)
+        }
+    }
 }
 
 const mutations = {
-   
+    STUDENTLIST(state,studentList) {
+        console.log(studentList);
+        state.studentList = studentList;//服务器返回的数据给仓库
+    },
 }
 
 const state = {
-    studentList:[],
+    studentList: [],
 }
-const getters={
-    
+const getters = {
+
 }
 export default {
     namespaced: true,//开启命名空间,modules里面的分类名才能被map四个方法捕捉到
